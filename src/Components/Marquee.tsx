08c8c926abaa8ad37d1eca7b5a2ccbb9f28620b7
Ediomojose <goodnewsediomojoshua@yahoo.com>
2024-01-25 00:31:14 +0100

    Created project folders and done importing logo

---
+++ src/Components/Marquee.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Google from '../assets/imgs/google-1-1.svg';
+import BlackRock from '../assets/imgs/blackrock-logo-1.svg';
+import CocaCola from '../assets/imgs/coca-cola-2021.svg';
+import JPMorganLogo from '../assets/imgs/jp-morgan.svg';
+import Microsoft from '../assets/imgs/microsoft-6.svg';
+import Nike from '../assets/imgs/nike-11.svg';
+import Nvidia from '../assets/imgs/nvidia.svg';
+import Reddit from '../assets/imgs/reddit-1.svg';
+import Tesla from '../assets/imgs/tesla-motors-1.svg'
+
+
+const Marquee = () => {
+  return (
+    <>
+    <main>
+      
+    </main>
+    </>
+  )
+}
+
+export default Marquee
